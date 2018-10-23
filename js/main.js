efe734a85c272e1cad37ce693f0d5583949edf7b
ron2man Ron Shtaiman <ron2man10@gmail.com>
2018-10-23 14:05:03 +0300

    ADD IMAGE2CANVASE

--- js/main.js
+++ js/main.js
@@ -5,10 +5,36 @@ var gCtx;
 
 function init() {
     var imgs = createImgs();
-    
+
     gCanvas = document.querySelector('#canvas');
     gCtx = gCanvas.getContext('2d');
-    
-    gCanvas.width = window.innerWidth/2;
-    gCanvas.height = window.innerHeight/2;
+
+    gCanvas.width = window.innerWidth / 2;
+    gCanvas.height = window.innerHeight / 2;
+
+    renderGallery()
+}
+
+
+// INJECT UL-> LI IMAGES TO GALLERY DIV
+function renderGallery() {
+    //TODO: get gallery function - instead of global var
+    var elGallery = document.querySelector('.gallery-imgs');
+    var images = gImgs;
+    var strHtml = '<ul>';
+    for (var i = 0; i < images.length; i++) {
+        strHtml += `    <li class="gallery-img">
+                        <img src="${images[i].url}" data-id="${images[i].id}" onclick="onSelectImg(this)">
+                        </li>`
+    }
+    strHtml += '</ul>'
+    elGallery.innerHTML = strHtml;
+}
+
+// gets image  Element
+// TO DO - UPDATE THE MODAL - WITH THE SELECTED IMAGE  
+function onSelectImg(image) {
+    drawImgOnCanvas(image);
+    console.log('Element',image);
+    console.log('Element Modal ID',image.dataset.id);
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -7,6 +7,16 @@ function createImgs() {
     gImgs = [
         createImg('img/003.jpg', ['happy']),
         createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
+        createImg('img/004.jpg', ['happy']),
     ];
 
     return gImgs;
@@ -14,4 +24,10 @@ function createImgs() {
 
 function createImg(url, keyWords) {
     return {id: createId(), url, keyWords};
+}
+
+function drawImgOnCanvas(image){
+    //TODO - CONNECT TO ANOTHER FUNC!
+    // HANDLE THE HEIGHT AND WIDTH - RESPONSIVE
+    gCtx.drawImage(image, 0, 0,458,315);
 }
