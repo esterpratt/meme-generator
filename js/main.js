82d0bcb2abb4bf071bac2646e16f04fae0f94c8a
Ester Pratt <esterpratt@gmail.com>
2018-10-23 14:08:28 +0300

    canvas play

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 var gCanvas;
 var gCtx;
+var gOffset;
 
 function init() {
     var imgs = createImgs();
@@ -11,4 +12,32 @@ function init() {
     
     gCanvas.width = window.innerWidth/2;
     gCanvas.height = window.innerHeight/2;
+
+    gOffset = getCanvasOffset();
+
+    createMeme();
+    
+}
+
+function getCanvasOffset() {
+    return {left: gCanvas.offsetLeft, top: gCanvas.offsetTop}
+}
+
+function onEnterText(txt) { 
+    var line = createLine(txt, 20, 0, 20, gCtx.fillStyle, 'left');
+
+    var meme = getMeme();
+
+    gCtx.fillText(txt, 0, 20);
+}
+
+function onChangeTextColor(color) {
+    gCtx.fillStyle = color;
+}
+
+function onClickCanvas(ev) {
+    var x = ev.clientX;
+    var y = ev.clientY;
+    console.log(x, y);
+
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -15,3 +15,27 @@ function createImgs() {
 function createImg(url, keyWords) {
     return {id: createId(), url, keyWords};
 }
+
+function createMeme() {
+    gMeme = {
+        selectedImgId: 1,
+        txts: []
+    };
+}
+
+function createLine(val, size, x, y, color, align) {
+    var line = {
+        line: val,
+        size: 30,
+        x: x,
+        y: y,
+        color: color, 
+        align: align
+    }
+
+    gMeme.txts.push(line);
+}
+
+function getMeme() {
+    return gMeme;
+}
