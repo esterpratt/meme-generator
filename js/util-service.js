2ba084a3a6a6adce29d41792c467294e4cb0abf2
Ester Pratt <esterpratt@gmail.com>
2018-10-25 09:56:00 +0300

    add localStorage functions

--- js/util-service.js
+++ js/util-service.js
@@ -12,6 +12,14 @@ function createId() {
     return txt;
 }
 
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
 // generates a random number between params min and max
 function generateNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
